d91dcd32f6407a54798e6336c9204bb59bed2964
Michael Wu <mwu1993@users.noreply.github.com>
2018-04-12 23:32:10 -0700

    update comments (#2)

--- demo_bot.py
+++ demo_bot.py
@@ -2,24 +2,28 @@ import random
 
 
 class SpyBot(object):
-    """This abstract class represents a bot that acts as a spy master for Codenames and can either give clues to the guesser
-     or be updated for game state or get a clue."""
+    """
+    This abstract class represents a bot that acts as a spy master for Codenames and can either be asked to
+    give a clue to the guesser or be updated about moves made in the game.
+    """
 
     def __init__(self, vocab, game_board, p_id):
-        """initialization for the bot
+        """
+        Initialization for the bot
         Args:
-            vocab (List<string>): list of English words that can be used to generate clues.
-            game_board (Map<string, Enum:WordType>): a dictionary representing a the game board, mapping the words on
+            vocab (List<string>): list of English words that can be used to generate clues. No other clue words allowed!
+            game_board (Map<string, Enum:WordType>): a dictionary representing the game board, mapping the words on
                 the board with an enum representing whether the word belongs to player 1, player 2, is neutral or a spy word.
+                See game_platform.py.
             p_id: (int): integer (0, 1) representing the the id of the bot, player 1 or 2.
         """
-        self.vocab = vocab
         pass
 
     def update(self, is_my_turn, clue_word, clue_num_guesses, guesses):
-        """Updates the bot with the changed game state after a round.
+        """
+        Informs the bot with what happened during a turn.
         Args:
-            is_my_turn (bool): True if it is the bot's turn to play or if it is the opponent's turn.
+            is_my_turn (bool): True if the turn was the bots or the opponent.
             clue_word (str): String representing the clue word in the round.
             clue_num_guesses (int): int representing the number of words that matched the given clue word.
             guesses: (List<string>): list of words that were guessed in the round.
@@ -27,14 +31,14 @@ class SpyBot(object):
         pass
 
     def getClue(self, invalid_words):
-        """Updates the bot with the changed game state after a round.
+        """
+        Gives a clue for the turn.
         Args:
-            invalid_words (Set<string>): set of words that are not allowed to be clued. Eg. words that share a root with
-                any of the words on the board.
+            invalid_words (Set<string>): set of words from the original vocab that are not allowed to be clued for this
+            turn. Eg. words that share a root with any of the face-up words on the board.
         Returns:
-            a tuple (clue, num_guesses) of a string representing the clue word which must come from
-                self.vocab and must not be in the set of invalid words, and an int representing the number of words
-                on the board that are supposed to that match the given clue.
+            a tuple (clue, num_guesses) where `clue` is a string clue from the given vocab and but not in the set of
+            invalid words, and `num_guesses` is the number of words on the board related to the clue.
         """
         pass
 
